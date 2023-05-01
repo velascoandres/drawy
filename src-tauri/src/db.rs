56ac031efff694c0ddae7008796e4f5677243f6f
Andres Velasco <velasco.andrs@gmail.com>
2023-05-01 17:51:43 -0500

    remove dotenv

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -1,14 +1,9 @@
-extern crate dotenv;
 extern crate diesel;
 
 use diesel::prelude::*;
-use dotenv::dotenv;
-use std::env;
 
 pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    SqliteConnection::establish(&database_url)
+    let database_url = "draws.db";
+    SqliteConnection::establish(database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
